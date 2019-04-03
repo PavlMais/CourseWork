eeaa9b5883633523755fda0a5880127eef7bd9a7
Pavlo Maistruk <pavlomajstruk@gmail.com>
2019-04-03 12:18:12 +0300

    Added methods toString

--- CourseWork/Data.h
+++ CourseWork/Data.h
@@ -8,6 +8,9 @@ struct User {
 	string login;
 	string password;
 	int group;
+	string toString(){
+		return login + "[l]" + password + "[p]" + std::to_string(group) + "[g]";  
+	}
 };
 
 struct Product {
@@ -20,6 +23,18 @@ struct Product {
 	float rating;
 	string description;
 	int id_company;
+    
+    string toString(){
+        string str = std::to_string(id) + "[id]";
+        str += type + "[t]" + name + "[n]";
+        str += std::to_string(price) + "[p]";
+        str += std::to_string(sale) + "[s]";
+        str += std::to_string(left_item) + "[l]";
+        str += std::to_string(rating) + "[r]";
+        str += description + "[d]";
+        std += std::to_string(id_company) + "[c]";
+        return str;
+    }
 };
 struct Company {
 	int id;
